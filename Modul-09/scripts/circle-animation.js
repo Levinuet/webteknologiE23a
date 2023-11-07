46de12211099fa668a3d783627054e5aebb10e87
user.name like levinuet <user.email like eaa23leej@students.eaaa.dk>
2023-11-07 15:08:31 +0100

    :)

--- Modul-09/scripts/circle-animation.js
+++ Modul-09/scripts/circle-animation.js
@@ -11,33 +11,42 @@ svg
   .attr("cx", 100)
   .attr("cy", 100)
   .attr("r", 50)
-  .attr("fill", "#49d6d1");
+  .attr("fill", "#49d6d1")
+  .attr("class","circle");
 
 d3.selectAll("circle")
   //En transition begynder
   .transition()
   //duration er hvor lang tid transitionen skal tage
-  .duration(2000)
+  .duration(800)
+  .attr("fill", "#fc60ea")
   //herunder definerer vi slutværdierne for transitionen - cirklens cx rykkes således fra 100 - 600
   .attr("cx", 600)
   //Nu strater vi endnu en trasition som skal bevæge cirklen tilbage til 100
   .transition()
   //duration er hvor lang tid transitionen skal tage
   .duration(2000)
+  .attr("fill", "#66ffbd")
   //herunder definerer vi slutværdierne for transitionen - cirklens cx rykkes således tilbage fra 600 - 100
   .attr("cx", 100);
 
-/**
- * Havde vi haft en knap et sted på siden, kunne vi have fået animationen til at starte ved klik på knappen:
- * d3.select("#klik")
+ //Havde vi haft en knap et sted på siden, kunne vi have fået animationen til at starte ved klik på knappen:
+ d3.select("#klik")
   .on("click", function () {
     d3.selectAll("circle")
       .transition() 
       .duration(2000)
+      .attr("fill", "#fc60ea")
       .attr("cx", 600)
       .transition()
       .duration(2000)
+      .attr("fill", "#53f5e7")
+      .attr("cy", 500)
+      .attr("cx", 300)
+      .transition()
+      .duration(2000)
+      .attr("fill", "#66ffbd")
       .attr("cx", 100)
+      .attr("cy", 100)
   });
-  * Bonusopgave: implementer en knap som starter animationen og få cirklens til at bevæge sig i en trekant.
- */
+  //Bonusopgave: implementer en knap som starter animationen og få cirklens til at bevæge sig i en trekant.
